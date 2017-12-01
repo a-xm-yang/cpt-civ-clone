37847c232d89f9d6180123c44e7c326b323f40d9
Nicholas Seniow <nicholas@seniow.com>
2017-12-01 11:16:52 -0500

    Test commit with Desert and Hills class

--- src/civilizationclone/Tile/Desert.java
+++ src/civilizationclone/Tile/Desert.java
@@ -1,10 +1,16 @@
 
 package civilizationclone.Tile;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 public class Desert extends Tile{
 
+    static Set<Improvement> possibleImprovement = EnumSet.of(Improvement.FARM, Improvement.RANCH, Improvement.ROAD);
+    
     public Desert() {
         super(false, 1);
+        
     }
 
     @Override
